a961a29b615e3343275bd6ebe3e3261928015f49
hirabarufumitaka <114893728+hirabarufumitaka@users.noreply.github.com>
2022-02-19 05:51:13 +0900

    Add:管理画面ログイン、トップページ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title(page_title = '', admin = false)
+    base_title = if admin
+                   'mobile game notification(管理画面)'
+                 else
+                   'mobile game notification'
+                 end
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
 end

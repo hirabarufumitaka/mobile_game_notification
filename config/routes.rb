f7b374695e4d6cd9c8197fdbac6dab7dafb3efac
Fumitaka-Hirabaru <hirabaru72@gmail.com>
2022-02-19 05:37:40 +0900

    Add:管理者用のコントローラー,ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
+
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
 end

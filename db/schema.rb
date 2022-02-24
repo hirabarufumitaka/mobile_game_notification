fcf09f254d80beb8dbb4482c0f6e29faa338d998
Fumitaka-Hirabaru <hirabaru72@gmail.com>
2022-02-25 05:00:23 +0900

    Add:genre,application,eventのモデルとマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_16_221555) do
+ActiveRecord::Schema.define(version: 2022_02_20_140815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.integer "game_application_id"
+    t.string "name"
+    t.text "description"
+    t.integer "event_type"
+    t.datetime "started_at", default: "2000-01-01 00:00:00", null: false
+    t.datetime "ended_at", default: "2000-01-01 01:00:00", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_application_id"], name: "index_events_on_game_application_id"
+  end
+
+  create_table "game_applications", force: :cascade do |t|
+    t.integer "game_genre_id"
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_genre_id"], name: "index_game_applications_on_game_genre_id"
+    t.index ["name"], name: "index_game_applications_on_name", unique: true
+  end
+
+  create_table "game_genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_game_genres_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false

848d1d16cab323ea9621094e565e63791524c918
hirabarufumitaka <114893728+hirabarufumitaka@users.noreply.github.com>
2022-02-25 05:17:52 +0900

    Add:管理画面のeventのcontrollerとroutes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
-    resources :events, only: %i[index edit update show destroy]
+    resources :events
     resources :users, only: %i[index edit update show destroy]
   end
 end

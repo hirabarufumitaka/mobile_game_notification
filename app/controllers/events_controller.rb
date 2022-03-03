73a632ef8d0be7fac8904edc29d5b526d074dbd2
hirabarufumitaka <114893728+hirabarufumitaka@users.noreply.github.com>
2022-03-03 11:53:59 +0900

    Add: 一覧画面のページネーション #5

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,7 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.all.order(started_at: :desc).page(params[:page])
   end
 
-  def show
-  end
+  def show; end
 end

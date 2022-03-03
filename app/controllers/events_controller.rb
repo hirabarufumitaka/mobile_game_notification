b93cab0ef034cafd39814a5272ceaf549a852c2a
hirabarufumitaka <114893728+hirabarufumitaka@users.noreply.github.com>
2022-03-03 15:56:56 +0900

    Add: event詳細画面 #5

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,5 +3,7 @@ class EventsController < ApplicationController
     @events = Event.all.order(started_at: :desc).page(params[:page])
   end
 
-  def show; end
+  def show
+    @event = Event.find_by(id: params[:id])
+  end
 end

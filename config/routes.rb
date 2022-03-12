f1b110b9a73ca9d83ebb9761d5c471ed6d221c99
Fumitaka-Hirabaru <hirabaru72@gmail.com>
2022-03-13 00:44:20 +0900

    add: 管理画面GameApplicationのCRUD #30

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+    resources :game_applications
     resources :events
     resources :users, only: %i[index edit update show destroy]
   end

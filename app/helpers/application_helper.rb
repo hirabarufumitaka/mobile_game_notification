689238552f5954a9af6857684a34b9c18b5ed2d1
Fumitaka-Hirabaru <hirabaru72@gmail.com>
2022-03-15 22:46:59 +0900

    add: metaタグの設定 #32

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,34 @@ module ApplicationHelper
   def active_if(path)
     path == controller_path ? 'active' : ''
   end
+
+  def default_meta_tags
+    {
+      site: 'ソシャゲイベント情報',
+      title: 'ソシャゲイベントをまとめて簡単に検索',
+      reverse: true,
+      separator: '|',
+      description: 'ソシャゲイベント情報は複数のソシャゲのイベントをまとめて簡単に探すことができるサイトです。',
+      keywords: 'ソシャゲ, イベント, プロセカ',
+      canonical: request.original_url,
+      icon: [
+        { href: image_url('logo.png'), sizes: '32x32' },
+        { href: image_url('logo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
+      ],
+      og: {
+        site_name: 'ソシャゲイベント情報',
+        title: 'ソシャゲイベントをまとめて簡単に検索',
+        description: 'ソシャゲイベント情報は複数のソシャゲのイベントをまとめて簡単に探すことができるサイトです。',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('logo.png')
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@barusrun',
+        title: 'ソシャゲイベントをまとめて簡単に検索',
+        image: image_url('logo.png')
+      }
+    }
+  end
 end

7b19c9ae472d1985c1f5b2447823e994a5fc9da9
hirabarufumitaka <114893728+hirabarufumitaka@users.noreply.github.com>
2022-04-14 14:07:25 +0900

    Add: ユーザー・通知・イベントモデルの設定 #44

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :authentications, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+  has_many :notification_events, through: :notifications, source: :event
   accepts_nested_attributes_for :authentications
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }

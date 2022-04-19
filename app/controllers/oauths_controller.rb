176c88509a1e03ff6713d8d98c7cabb67873d41f
hirabarufumitaka <114893728+hirabarufumitaka@users.noreply.github.com>
2022-04-19 22:37:16 +0900

    Fix: LINEログインした時にイベント一覧に遷移 #44

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -6,15 +6,15 @@ class OauthsController < ApplicationController
   def callback
     provider = auth_params[:provider]
     if (@user = login_from(provider))
-      redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+      redirect_to events_path, notice: "#{provider.titleize}でログインしました"
     else
       begin
         @user = create_from(provider)
         reset_session
         auto_login(@user)
-        redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+        redirect_to events_path, notice: "#{provider.titleize}でログインしました"
       rescue StandardError
-        redirect_to root_path, alert: "#{provider.titleize}でのログインに失敗しました"
+        redirect_to events_path, alert: "#{provider.titleize}でのログインに失敗しました"
       end
     end
   end

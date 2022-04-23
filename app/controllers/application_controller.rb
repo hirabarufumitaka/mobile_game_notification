a9711c7ac8fe8ed636edf0996a90b8e93fb744fb
Fumitaka-Hirabaru <hirabaru72@gmail.com>
2022-04-23 19:58:41 +0900

    Fix: basic認証の解除 #46

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
-
-  before_action :basic
-
-  def basic
-    return unless Rails.env == 'production'
-
-    authenticate_or_request_with_http_basic do |name, password|
-      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
-  end
 end
